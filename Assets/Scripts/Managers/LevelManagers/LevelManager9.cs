ee081a56c556ac6fcd39f9c455aa0043852042ca
Sebastien Gaumin <sgaumin@gmail.com>
2019-01-22 16:05:46 +0100

    Setup Last Scene

---
+++ Assets/Scripts/Managers/LevelManagers/LevelManager9.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager9 : MonoBehaviour
+{
+    [SerializeField] private Animator mathiasAnimator;
+    [SerializeField] private Animator henryAnimator;
+
+    [SerializeField] private Animator background;
+
+    // UI
+    [SerializeField] private GameObject dialogues;
+    [SerializeField] private GameObject nameDialogues;
+
+    private Character mathiasCharacter;
+    private Character henryCharacter;
+
+    private DialogueBox dialogueBox;
+    private int indexCount;
+
+    private bool isStarting = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Deactivate fad in animation for Mathias
+        mathiasAnimator.SetBool("IsFadIn", false);
+
+        // Asign Character components
+        mathiasCharacter = mathiasAnimator.gameObject.GetComponent<Character>();
+        henryCharacter = henryAnimator.gameObject.GetComponent<Character>();
+
+        // Asign DialogueBox component
+        dialogueBox = dialogues.GetComponent<DialogueBox>();
+
+        // Hide
+        henryCharacter.gameObject.SetActive(false);
+        background.gameObject.SetActive(false);
+
+        // Index for checking the current IndexDialogue of DialogueSystemScript script 
+        indexCount = 999;
+
+        // Hide Dialogue Box
+        ShowDialogues(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set Animation and Sound according to the Dialogue Index
+        if (DialogueSystemScript.indexDialogue == indexCount)
+            return;
+
+        indexCount = DialogueSystemScript.indexDialogue;
+
+        if ((indexCount == 10 || indexCount == 0) && isStarting)
+        {
+            //DialogueSystemScript.indexDialogue = 0; // TO report
+            indexCount = 0;
+            StartCoroutine(StartLevel());
+            isStarting = false;
+        }
+
+        // henry Speaking Steps
+        if (indexCount == 1)
+            StartCoroutine(henryTalking());
+
+        // End Level
+        if (indexCount == 2)
+            StartCoroutine(FinalStepLevel());
+    }
+
+    IEnumerator StartLevel()
+    {
+        yield return new WaitForSeconds(1f);
+
+        // Show Background
+        background.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        // Show henry Character
+        henryCharacter.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        // Show dialogues box 
+        ShowDialogues(true);
+        yield return new WaitForSeconds(0.5f);
+
+        // Show Texts into the dialogues box
+        dialogueBox.ShowTexts(true);
+
+        // Coroutine End
+        yield break;
+    }
+
+    IEnumerator henryTalking()
+    {
+        // Set henry  talking animation
+        henryAnimator.SetTrigger("BackTalking");
+
+        // Coroutine End
+        yield break;
+    }
+
+    IEnumerator FinalStepLevel()
+    {
+        // Set henry idle animation
+        henryAnimator.SetTrigger("ResetBack");
+        yield return new WaitForSeconds(0.5f);
+
+        // Set Mathias talking animation
+        mathiasAnimator.SetTrigger("Talking");
+        yield return new WaitForSeconds(1f);
+
+        // Reset Mathias animation
+        mathiasAnimator.SetTrigger("Reset");
+        yield return new WaitForSeconds(1f);
+
+        // henry Fad Out animation
+        henryAnimator.SetTrigger("FadOut");
+
+        // Hide Texts into the dialogues box
+        dialogueBox.ShowTexts(false);
+        yield return new WaitForSeconds(0.5f);
+
+        // Animation FadOut dialogues box
+        Animator dialoguesAnimator = dialogues.GetComponent<Animator>();
+        dialoguesAnimator.SetTrigger("FadOut");
+        Animator dialoguesNameAnimator = nameDialogues.GetComponent<Animator>();
+        dialoguesNameAnimator.SetTrigger("FadOut");
+
+        yield return new WaitForSeconds(0.5f);
+
+        // Background fad out animation
+        background.SetTrigger("FadOut");
+        yield return new WaitForSeconds(2f);
+
+        // Coroutine End
+        GameSystem.instance.PlayNextScene();
+        yield break;
+    }
+
+    void ShowDialogues(bool activated)
+    {
+        if (dialogues != null)
+        {
+            if (activated)
+            {
+                dialogues.SetActive(true);
+                nameDialogues.SetActive(true);
+            }
+
+            if (!activated)
+            {
+                dialogues.SetActive(false);
+                nameDialogues.SetActive(false);
+            }
+        }
+    }
+}

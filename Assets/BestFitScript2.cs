ebc59be0683195649688ad35e0710866d5dd3550
phenixapoil <35372381+phenixapoil@users.noreply.github.com>
2019-01-23 10:25:09 +0100

    Best Fit Malle

---
+++ Assets/BestFitScript2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BestFitScript2 : MonoBehaviour
+{
+    public GameObject BestFitObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Text>().fontSize = BestFitObject.GetComponent<Text>().cachedTextGenerator.fontSizeUsedForBestFit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Text>().fontSize = BestFitObject.GetComponent<Text>().cachedTextGenerator.fontSizeUsedForBestFit;
+    }
+}

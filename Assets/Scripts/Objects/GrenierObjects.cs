43447b91902fae425e1a1aee7c4865a9c033cadb
Sebastien Gaumin <sgaumin@gmail.com>
2019-08-17 16:55:49 +0200

    [Feature] Translations in scene 8 implemented.

--- Assets/Scripts/Objects/GrenierObjects.cs
+++ Assets/Scripts/Objects/GrenierObjects.cs
@@ -3,8 +3,22 @@ using UnityEngine.UI;
 
 public class GrenierObjects : MonoBehaviour
 {
+	[Header("Comment Translations")]
 	[TextArea(3, 10)]
-	[SerializeField] private string comment;
+	[SerializeField] private string frenchComment;
+	[TextArea(3, 10)]
+	[SerializeField] private string englishComment;
+	[TextArea(3, 10)]
+	[SerializeField] private string japaneseComment;
+
+	[Header("Letter content Translations")]
+	[TextArea(3, 10)]
+	[SerializeField] private string frenchLetterContent;
+	[TextArea(3, 10)]
+	[SerializeField] private string englishLetterContent;
+	[TextArea(3, 10)]
+	[SerializeField] private string japaneseLetterContent;
+
 	[SerializeField] private bool isLetter;
 	[SerializeField] private bool isHenriLetter;
 	[SerializeField] private bool canBeSelected;
@@ -13,6 +27,8 @@ public class GrenierObjects : MonoBehaviour
 
 	private LevelManager8 levelManager;
 	private Animator animator;
+	private string comment;
+	private string letterContent;
 
 	public bool IsChecked { get; private set; }
 
@@ -21,6 +37,22 @@ public class GrenierObjects : MonoBehaviour
 		levelManager = FindObjectOfType<LevelManager8>();
 		animator = GetComponent<Animator>();
 		canBeSelected = true;
+
+		switch (LanguageData.Language)
+		{
+			case Languages.French:
+				comment = frenchComment;
+				letterContent = frenchLetterContent;
+				break;
+			case Languages.English:
+				comment = englishComment;
+				letterContent = englishLetterContent;
+				break;
+			case Languages.Japanese:
+				comment = japaneseComment;
+				letterContent = japaneseLetterContent;
+				break;
+		}
 	}
 
 	public bool CanBeSelected
@@ -79,13 +111,11 @@ public class GrenierObjects : MonoBehaviour
 			IsChecked = true;
 			if (isLetter)
 			{
-				lettersBox.text = comment;
+				lettersBox.text = letterContent;
 				StartCoroutine(levelManager.ShowLetter(isHenriLetter));
 			}
-			else
-			{
-				commentsBox.text = comment;
-			}
+
+			commentsBox.text = comment;
 		}
 	}
 }
